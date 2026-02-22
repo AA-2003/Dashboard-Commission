bf50dc7e1fcf8dd7dfe49e4da1ab9ad7475d63ff
AA-2003 <alireza.azizkhani.2003@gmail.com>
2026-02-22 11:18:06 +0330

    test loading data from didar CRM

--- main.py
+++ main.py
@@ -2,7 +2,31 @@ import streamlit as st
 
 from utils.custom_css import apply_custom_css
 from utils.sidebar import render_sidebar
+
+
 COMMISSION_DASHBOARD = "داشبورد عملکرد تیم ها"
+
+def load_didar_date():
+    """test loading data from didar in streamlit servers"""
+    import requests
+    api_key = st.secrets.get("GENERAL").get("DIDAR_API_KEY", "")
+    url = f"https://app.didar.me/api/deal/search?apikey={api_key}"
+    try:
+        payload = {
+            "Criteria": {
+                "ContactIds":["4d6157b5-a8aa-4d0e-b203-1da3f19e3dc2"]},
+            "From":0,
+            "Limit":1000
+        }
+        res = requests.post(url, json=payload)
+        res.raise_for_status()
+
+        data = res.json()
+        st.write(data)
+    except Exception as e:
+        st.error(f"خطا در بارگذاری داده‌ها از دیدار: {e}")
+        return None
+    
 
 def main():
     """Main function to run the Streamlit app."""
@@ -11,6 +35,9 @@ def main():
         layout="wide",
     )
     apply_custom_css()
+
+    # test loading data from didar in streamlit servers
+    load_didar_date()
 
     # logo
     st.image("static/logo.svg", width=300)
