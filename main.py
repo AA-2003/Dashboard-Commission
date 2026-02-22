ba9ac77a1a4c3555869cfb7e55a6278203a1b52d
AA-2003 <alireza.azizkhani.2003@gmail.com>
2026-02-22 11:26:27 +0330

    cant load didar data from streamlit servers

--- main.py
+++ main.py
@@ -6,26 +6,6 @@ from utils.sidebar import render_sidebar
 
 COMMISSION_DASHBOARD = "داشبورد عملکرد تیم ها"
 
-def load_didar_date():
-    """test loading data from didar in streamlit servers"""
-    import requests
-    api_key = st.secrets.get("GENERAL").get("DIDAR_API_KEY", "")
-    url = f"https://app.didar.me/api/deal/search?apikey={api_key}"
-    try:
-        payload = {
-            "Criteria": {
-                "ContactIds":["4d6157b5-a8aa-4d0e-b203-1da3f19e3dc2"]},
-            "From":0,
-            "Limit":1000
-        }
-        res = requests.post(url, json=payload)
-        res.raise_for_status()
-
-        data = res.json()
-        st.write(data)
-    except Exception as e:
-        st.error(f"خطا در بارگذاری داده‌ها از دیدار: {e}")
-        return None
     
 
 def main():
@@ -36,9 +16,7 @@ def main():
     )
     apply_custom_css()
 
-    # test loading data from didar in streamlit servers
-    load_didar_date()
-
+    # we cant load didar data from streamlit servers, so we use google sheet 
     # logo
     st.image("static/logo.svg", width=300)
     
